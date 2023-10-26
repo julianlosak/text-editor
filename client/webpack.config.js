563714ed9a33ef2764325262d5c038f42a7f2e82
julianlosak <julianlosak@gmail.com>
2023-10-25 23:46:46 -0500

    updated icon path

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = () => {
         theme_color: '#31a9e1',
         icons: [
           {
-            src: path.resolve('src/img/icon.png'),
+            src: path.resolve('src/favicon.ico'),
             sizes: [96, 128, 192, 256, 384, 512],
           },
         ],
